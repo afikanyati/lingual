585aa8d33ee976c9c46eb5a598ddcd644a9bae77
Afika Nyati <afika.a.nyati@gmail.com>
2020-06-09 21:33:44 -0400

    Initial Commit

---
+++ diction-processor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  diction-processor
+//
+//  Created by Afika Nyati on 6/9/20.
+//  Copyright © 2020 Afika Nyati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ diction-processorTests/diction_processorTests.swift
@@ -0,0 +1,34 @@
+//
+//  diction_processorTests.swift
+//  diction-processorTests
+//
+//  Created by Afika Nyati on 6/9/20.
+//  Copyright © 2020 Afika Nyati. All rights reserved.
+//
+
+import XCTest
+@testable import diction_processor
+
+class diction_processorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
